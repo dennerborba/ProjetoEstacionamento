34fcab8f2a7f0e3170ed593fdf8bf148ac5b9eb8
dennerborba <dennerdeborba@gmail.com>
2025-04-01 17:18:54 -0300

    add

---
+++ ControleEstacionamento.Utils/Database/DBConnection.cs
@@ -0,0 +1,25 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace ControleEstacionamento.Utils.Database
+{
+    public class DBConnection
+    {
+        public const string CONNECTION_STRING = "Server=localhost;Database=estacionamento;User ID=root;Password=root;";
+        
+        public static bool TestarConexao()
+        {
+            try
+            {
+                using (MySqlConnection conn  = new MySqlConnection(CONNECTION_STRING))
+                {
+                    conn.Open();
+                    return true;
+                }
+            } 
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ControleEstacionamento/Models/Ticket/TicketModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ControleEstacionamento.Models.Ticket
+{
+    public class TicketModel
+    {
+        public long ID { get; set; }
+        public string Placa_carro { get; set; }
+        public DateTime? Hora_chegada { get; set; }
+        public DateTime? Hora_saida { get; set; }
+        public float Preco {  get; set; }
+
+        public TicketModel()
+        {
+            
+        }
+
+    }
+}
